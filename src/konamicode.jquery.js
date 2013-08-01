d0bd7de32155a2c861624591bdda906664e1e438
ddprrt <stefan.baumgartner@netural.com>
2013-08-01 11:12:11 +0200

    added bower.json + test.html
    and some other cleanups as well!

--- src/konamicode.jquery.js
+++ src/konamicode.jquery.js
@@ -1,19 +1,19 @@
 /**
 
-	Author: Stefan Baumgartner (@ddprrt)
-	File: konamicode.jquery.js
-	Requires: konamicode.js, jQuery
+    Author: Stefan Baumgartner (@ddprrt)
+    File: konamicode.jquery.js
+    Requires: konamicode.js, jQuery
 
-	Description: Small jQuery extension for the konamicode.js
+    Description: Small jQuery extension for the konamicode.js
 
 **/
 
 !function ($, window, undefined) {
 
-	$.fn.konamicode = function(callback) {
-		if(window.konamicode) {
-			window.konamicode(this[0], callback);
-		}
-	}
+    $.fn.konamicode = function(callback) {
+        if(window.konamicode) {
+            window.konamicode(this[0], callback);
+        }
+    }
 
 }(jQuery, window)
--- src/konamicode.js
+++ src/konamicode.js
@@ -1,71 +1,71 @@
 /**
 
-	Author: Stefan Baumgartner (@ddprrt)
-	File: konamicode.js
+    Author: Stefan Baumgartner (@ddprrt)
+    File: konamicode.js
 
-	Description: Adds the konamicode to any element on your website. You can add a multitude
-	of callbacks.
-	
+    Description: Adds the konamicode to any element on your website. You can add a multitude
+    of callbacks.
+    
 **/
 
-!function (window, undefined) {
+;(function (window, undefined) {
 
-	'use strict';
+    'use strict';
 
-	var keyCodes = [38,38,40,40,37,39,37,39,66,65],
-		elems = {};
+    var keyCodes = [38,38,40,40,37,39,37,39,66,65],
+        elems = {};
 
-	window.konamicode = function(elem, callback) {
-		if(!elems[elem]) {
-			elems[elem] = new KonamiCode(elem);
-		}
+    window.konamicode = function(elem, callback) {
+        if(!elems[elem]) {
+            elems[elem] = new KonamiCode(elem);
+        }
 
-		elems[elem].addCallback(callback);
-	}
+        elems[elem].addCallback(callback);
+    }
 
-	function KonamiCode(elem) {
-		var that = this;
-		that.elem = elem;
-		if (that.elem.addEventListener) {  
-			that.elem.addEventListener('keyup', check, false);   
-		} else if (that.elem.attachEvent)  {  
-			that.elem.attachEvent('onkeyup', check);  
-		} 
+    function KonamiCode(elem) {
+        var that = this;
+        that.elem = elem;
+        if (that.elem.addEventListener) {
+            that.elem.addEventListener('keyup', check, false);   
+        } else if (that.elem.attachEvent) {
+            that.elem.attachEvent('onkeyup', check);  
+        } 
 
-		function check(ev) {
-			that.checkPosition(ev);
-		}
-	}
+        function check(ev) {
+            that.checkPosition(ev);
+        }
+    }
 
-	KonamiCode.prototype = {
-		position: 0,
-		callbacks: [],
-		addCallback: function(callback) {
-			this.callbacks.push(callback);
-		},
-		executeCallbacks: function() {
-			var i = 0,
-				len = this.callbacks.length;
+    KonamiCode.prototype = {
+        position: 0,
+        callbacks: [],
+        addCallback: function(callback) {
+            this.callbacks.push(callback);
+        },
+        executeCallbacks: function() {
+            var i = 0,
+                len = this.callbacks.length;
 
-			for(i; i < len; i = i + 1) {
-				if(this.callbacks[i]) {
-					this.callbacks[i]();
-				}
-			}
+            for(i; i < len; i = i + 1) {
+                if(this.callbacks[i]) {
+                    this.callbacks[i]();
+                }
+            }
 
-			this.position = 0;
-		},
-		checkPosition: function(ev) {
-			if(keyCodes[this.position] === ev.keyCode) {
-				if(this.position+1 < keyCodes.length) {
-					this.position = this.position + 1;
-				} else {
-					this.executeCallbacks();
-				}
-			} else {
-				this.position = 0;
-			}
-		}
-	}
+            this.position = 0;
+        },
+        checkPosition: function(ev) {
+            if(keyCodes[this.position] === ev.keyCode) {
+                if(this.position+1 < keyCodes.length) {
+                    this.position = this.position + 1;
+                } else {
+                    this.executeCallbacks();
+                }
+            } else {
+                this.position = 0;
+            }
+        }
+    }
 
-}(window)
+})(window)
